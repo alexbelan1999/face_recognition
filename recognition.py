8296299ecfc42aea61e268944bd57ccf408d0f40
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-06 18:02:46 +0300

    Add for cycle

--- recognition.py
+++ recognition.py
@@ -1,40 +1,43 @@
 import face_recognition
-from PIL import Image, ImageDraw
 import numpy as np
 import image_to_recognition
 import cv2
 import datetime
+import glob
 
 known_face_encodings = []
 known_face_names = []
 image_to_recognition.images(known_face_encodings, known_face_names)
 
-unknown_image = face_recognition.load_image_file("testphoto/bill_elon_steve1.jpg")
-img = cv2.imread("testphoto/bill_elon_steve1.jpg")
-face_locations = face_recognition.face_locations(unknown_image)
-face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
-names = []
+for file in glob.glob("testphoto/*"):
+    unknown_image = face_recognition.load_image_file(file)
+    img = cv2.imread(file)
 
-for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    name = "Unknown"
-    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-    best_match_index = np.argmin(face_distances)
-    if matches[best_match_index]:
-        name = known_face_names[best_match_index]
-        names.append(name)
+    face_locations = face_recognition.face_locations(unknown_image)
+    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+    names = []
 
-    cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 3)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
-    cv2.rectangle(img, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+            names.append(name)
+        else:
+            names.append(name)
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 3)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
+        cv2.rectangle(img, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
 
-    cv2.putText(img, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
-
-basename = ""
-for i in names:
-    basename += str(i) + '_'
-suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
-filename = "".join([basename, suffix])
-path = "savephoto/" + filename + '.jpg'
-cv2.imwrite(path, img)
+        cv2.putText(img, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
+    print(file," ",names)
+    basename = ""
+    for i in names:
+        basename += str(i) + '_'
+    suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
+    filename = "".join([basename, suffix])
+    path = "savephoto/" + filename + '.jpg'
+    cv2.imwrite(path, img)
