af65fbd0f0e9cd6cfdcdded98999c0534cc838d2
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-06 16:51:34 +0300

    Calculate 1 photo

--- image_to_recognition.py
+++ image_to_recognition.py
@@ -1,10 +1,10 @@
 import glob
 import os
+
 import face_recognition
 
-known_face_encodings = []
-known_face_names = []
-def images(known_face_encodings ,known_face_names ):
+
+def images(known_face_encodings, known_face_names):
     for file in glob.glob("person/*"):
         image = face_recognition.load_image_file(file)
         known_face_encodings.append(face_recognition.face_encodings(image)[0])
