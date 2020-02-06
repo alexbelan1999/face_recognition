6ccf35bdb8eecaeb62fc10214c62ec4e2f43961a
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-06 18:44:40 +0300

    Add work with web camera

--- web_camera_recognition.py
+++ web_camera_recognition.py
@@ -9,12 +9,15 @@ known_face_encodings = []
 known_face_names = []
 image_to_recognition.images(known_face_encodings, known_face_names)
 
-for file in glob.glob("testphoto/*"):
-    unknown_image = face_recognition.load_image_file(file)
-    img = cv2.imread(file)
+video_capture = cv2.VideoCapture(0)
 
-    face_locations = face_recognition.face_locations(unknown_image)
-    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+while True:
+    ret, frame = video_capture.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = frame[:, :, ::-1]
+
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
     names = []
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
@@ -23,18 +26,17 @@ for file in glob.glob("testphoto/*"):
 
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
-
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
         names.append(name)
 
-        cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 3)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
         font = cv2.FONT_HERSHEY_SIMPLEX
         [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
-        cv2.rectangle(img, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
-        cv2.putText(img, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
+        cv2.rectangle(frame, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
+        cv2.putText(frame, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
 
-    print(file, " ", names)
+    print("Person: ", names)
     basename = ""
     for i in names:
         basename += str(i) + '_'
@@ -44,4 +46,12 @@ for file in glob.glob("testphoto/*"):
     path = "savephoto/" + filename + '.jpg'
 
     if len(names) > 0:
-        cv2.imwrite(path, img)
+        cv2.imwrite(path, frame)
+
+    cv2.imshow('Video', frame)
+    key = cv2.waitKey(100)
+    if key == 27:
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
