907388cf6f2bcad2595fadc6be7732b4e87260e2
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-06 19:50:56 +0300

    Test recognition

--- recognition.py
+++ recognition.py
@@ -4,20 +4,23 @@ import image_to_recognition
 import cv2
 import datetime
 import glob
+import time
 
+clock1 = time.time()
 known_face_encodings = []
 known_face_names = []
 image_to_recognition.images(known_face_encodings, known_face_names)
-
+print("Time 1:",time.time()-clock1)
 for file in glob.glob("testphoto/*"):
+    clock3 = time.time()
     unknown_image = face_recognition.load_image_file(file)
     img = cv2.imread(file)
-
     face_locations = face_recognition.face_locations(unknown_image)
     face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
     names = []
-
+    print("Time 2:",time.time()-clock3)
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        clock4  = time.time()
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
         name = "Unknown"
 
@@ -33,7 +36,9 @@ for file in glob.glob("testphoto/*"):
         [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
         cv2.rectangle(img, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
         cv2.putText(img, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
+        print("Time 3:",time.time()-clock4)
 
+    clock5 = time.time()
     print(file, " ", names)
     basename = ""
     for i in names:
@@ -45,3 +50,9 @@ for file in glob.glob("testphoto/*"):
 
     if len(names) > 0:
         cv2.imwrite(path, img)
+    print("Time 4:",time.time()-clock5)
+    print("Time 5:", time.time() - clock3)
+
+
+clock2 = time.time()
+print("All time:", clock2 - clock1)
