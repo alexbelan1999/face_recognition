118b797c42d5fd930658bbee8a3dfdfa00431670
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-07 16:41:05 +0300

    Add insert function

--- postgresql.py
+++ postgresql.py
@@ -1,24 +1,26 @@
 import psycopg2
 
-exit = True
-connection = None
-try:
-    print("Загрузка данных на сервер.")
-    sql = "INSERT INTO public.persons (name, addtime) VALUES ('Alex_Belan', '2020-02-07 16:07:54');"
 
-    connection = psycopg2.connect(dbname='testdb', user='postgres', password='1234', host='127.0.0.1')
-    with connection.cursor() as cursor:
+def insert(pname, time):
+    exit = True
+    connection = None
+    try:
+        print("Загрузка данных на сервер.")
+        sql = "INSERT INTO public.persons (name, addtime) VALUES ('" + pname + "', '" + time + "');"
 
-        cursor.execute(sql)
-        connection.commit()
-        cursor.close()
+        connection = psycopg2.connect(dbname='testdb', user='postgres', password='1234', host='127.0.0.1')
+        with connection.cursor() as cursor:
+
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
 
 
-except psycopg2.OperationalError:
-    print("Ошибка соединения с базой данных!")
-    exit = False
+    except psycopg2.OperationalError:
+        print("Ошибка соединения с базой данных!")
+        exit = False
 
-finally:
-    if exit == True:
-        connection.close()
-        print("Соединение закрыто")
+    finally:
+        if exit == True:
+            connection.close()
+            print("Соединение закрыто")
