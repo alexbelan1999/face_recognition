594af9eff60483ffbb7a9acbc812adf43cea502e
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-13 12:00:45 +0300

    Add main frame

--- video_recognition.py
+++ video_recognition.py
@@ -15,51 +15,57 @@ known_face_names = []
 image_to_recognition.images(known_face_encodings, known_face_names)
 frame_number = 0
 
+seconds = 0.5
+fps = input_movie.get(cv2.CAP_PROP_FPS)
+multiplier = round(fps * seconds)
+
 while True:
     ret, frame = input_movie.read()
     frame_number += 1
 
     if not ret:
         break
+    print(frame_number," ",multiplier," ",frame_number % multiplier)
+    if frame_number % multiplier == 0:
+        print(frame_number)
+        rgb_frame = frame[:, :, ::-1]
 
-    rgb_frame = frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        names = []
 
-    names = []
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Unknown"
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+            names.append(name)
 
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-        names.append(name)
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
+            cv2.rectangle(frame, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
+            cv2.putText(frame, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
 
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
-        cv2.rectangle(frame, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
-        cv2.putText(frame, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
+        print("Person: ", names)
+        basename = ""
+        persons = []
+        suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        for i in names:
+            basename += str(i) + '_'
+            persons.append([str(i), timestamp])
 
-    print("Person: ", names)
-    basename = ""
-    persons = []
-    suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    for i in names:
-        basename += str(i) + '_'
-        persons.append([str(i), timestamp])
+        filename = "".join([basename, suffix])
+        path = "savephoto/" + filename + '.jpg'
 
-    filename = "".join([basename, suffix])
-    path = "savephoto/" + filename + '.jpg'
-
-    if len(names) > 0:
-        cv2.imwrite(path, frame)
-        pg.insert(persons)
+        if len(names) > 0:
+            cv2.imwrite(path, frame)
+            pg.insert(persons)
 
     print("Обработка  {} / {}".format(frame_number, length))
 
