92ed1b15f572a2669b49642bf9ff755151694d90
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-14 11:21:15 +0300

    Add work with pickle for web camera

--- web_camera_recognition.py
+++ web_camera_recognition.py
@@ -5,15 +5,22 @@ import cv2
 import datetime
 import time
 import postgresql as pg
+import dump_and_load_pickle as dalp
 
-known_face_encodings = []
-known_face_names = []
-image_to_recognition.images(known_face_encodings, known_face_names)
+# known_face_encodings = []
+# known_face_names = []
+
+# image_to_recognition.images(known_face_encodings, known_face_names)
+# dalp.dump(known_face_encodings,"encodings1")
+# dalp.dump(known_face_names,"names1")
+known_face_encodings = dalp.load("encodings1")
+known_face_names = dalp.load("names1")
 
 video_capture = cv2.VideoCapture(0)
 
 while True:
     clock1 = time.time()
+
     ret, frame = video_capture.read()
     frame = cv2.flip(frame, 1)
     rgb_frame = frame[:, :, ::-1]
@@ -38,11 +45,17 @@ while True:
         cv2.rectangle(frame, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
         cv2.putText(frame, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
 
+    if len(names) == 0:
+        continue
+
     print("Person: ", names)
+
     basename = ""
     persons = []
+
     suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     for i in names:
         basename += str(i) + '_'
         persons.append([str(i), timestamp])
