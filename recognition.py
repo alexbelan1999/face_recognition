cae077b542e1be27a907f770e175891f8bda6a9a
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-14 10:12:18 +0300

    Remove clock from recognition

--- recognition.py
+++ recognition.py
@@ -8,20 +8,21 @@ import time
 import postgresql as pg
 
 clock1 = time.time()
+
 known_face_encodings = []
 known_face_names = []
+
 image_to_recognition.images(known_face_encodings, known_face_names)
-print("Time 1:", time.time() - clock1)
+
 for file in glob.glob("testphoto/*"):
-    clock3 = time.time()
     unknown_image = face_recognition.load_image_file(file)
     img = cv2.imread(file)
+
     face_locations = face_recognition.face_locations(unknown_image)
     face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
     names = []
-    print("Time 2:", time.time() - clock3)
+
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        clock4 = time.time()
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
         name = "Unknown"
 
@@ -37,14 +38,17 @@ for file in glob.glob("testphoto/*"):
         [(width, height), baseline] = cv2.getTextSize(name, font, 1.0, 3)
         cv2.rectangle(img, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
         cv2.putText(img, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
-        print("Time 3:", time.time() - clock4)
 
-    clock5 = time.time()
+    if len(names)==0:
+        continue
+
     print(file, " ", names)
     basename = ""
     persons = []
+
     suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     for i in names:
         basename += str(i) + '_'
         persons.append([str(i), timestamp])
@@ -55,9 +59,6 @@ for file in glob.glob("testphoto/*"):
     if len(names) > 0:
         cv2.imwrite(path, img)
         pg.insert(persons)
-
-    print("Time 4:", time.time() - clock5)
-    print("Time 5:", time.time() - clock3)
 
 clock2 = time.time()
 print("All time:", clock2 - clock1)
