15d3de409ab831ae0500aa93d990896a68936392
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-14 11:33:23 +0300

    Add work with pickle for video

--- video_recognition.py
+++ video_recognition.py
@@ -5,17 +5,23 @@ import cv2
 import datetime
 import time
 import postgresql as pg
+import dump_and_load_pickle as dalp
 
 clock1 = time.time()
 input_movie = cv2.VideoCapture("Bill.mp4")
 length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
 
-known_face_encodings = []
-known_face_names = []
-image_to_recognition.images(known_face_encodings, known_face_names)
+# known_face_encodings = []
+# known_face_names = []
+
+# image_to_recognition.images(known_face_encodings, known_face_names)
+# dalp.dump(known_face_encodings,"encodings1")
+# dalp.dump(known_face_names,"names1")
+known_face_encodings = dalp.load("encodings1")
+known_face_names = dalp.load("names1")
 frame_number = 0
 
-seconds = 0.5
+seconds = 1
 fps = input_movie.get(cv2.CAP_PROP_FPS)
 multiplier = round(fps * seconds)
 
@@ -25,9 +31,7 @@ while True:
 
     if not ret:
         break
-    print(frame_number," ",multiplier," ",frame_number % multiplier)
     if frame_number % multiplier == 0:
-        print(frame_number)
         rgb_frame = frame[:, :, ::-1]
 
         face_locations = face_recognition.face_locations(rgb_frame)
@@ -41,6 +45,7 @@ while True:
 
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
+
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
             names.append(name)
@@ -51,11 +56,16 @@ while True:
             cv2.rectangle(frame, (left + 3, bottom - height - 8), (left + width + 5, bottom - 3), (0, 0, 0), cv2.FILLED)
             cv2.putText(frame, name, (left + 5, bottom - 5), font, 1.0, (255, 255, 0), 3)
 
+        if len(names) == 0:
+            continue
+
         print("Person: ", names)
         basename = ""
         persons = []
+
         suffix = datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         for i in names:
             basename += str(i) + '_'
             persons.append([str(i), timestamp])
@@ -71,4 +81,4 @@ while True:
 
 input_movie.release()
 cv2.destroyAllWindows()
-print("Time:",time.time()-clock1)
+print("Time:", time.time() - clock1)
